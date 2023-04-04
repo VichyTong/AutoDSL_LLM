9566aa073ab2ccf94d35d77eb8f3c2bb5392cec3
ted <vichytong@163.com>
2023-04-04 15:51:07 +0800

    [feat] add relations generation.

--- src/baseline1/main.py
+++ src/baseline1/main.py
@@ -23,7 +23,6 @@ def get_reagents(reagents_text):
         presence_penalty=0.0,
         stop=["END"]
     )
-    print(response['choices'][0]['message']['content'])
     return response['choices'][0]['message']['content'].split(", ")
 
 
@@ -45,27 +44,46 @@ def get_equipments(equipments_text):
         presence_penalty=0.0,
         stop=["END"]
     )
-    print(response['choices'][0]['message']['content'])
     return response['choices'][0]['message']['content'].split(", ")
+
+
+def parse_relation_response(relation_str):
+    res = []
+    tmp = relation_str.split('), (')
+    for x in tmp:
+        res.append(x.split(' | '))
+    return res
 
 
 def get_relations(reagents, equipments, procedure):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "system", "content": "You are a helpful assistant to extrapolate as many relationships as possible from the Procedure in a form of (entity, relation, entity)."},
+            {"role": "system", "content": "You are a helpful assistant to extrapolate as many relationships as possible from the Procedure in a form of (entity | relation | entity)."},
+            {"role": "user", "content": "Reagents List: " + "Pfu buffer, upstream and downstream primer, dNTPs mix, pXa-1, DMSO, Pfu DNA polymerase, autoclaved ddH<sub>2</sub>O"},
+            {"role": "user", "content": "Equipments List: " + ""},
+            {"role": "user", "content": "Procedure: " + "Polymerase chain reaction. The PSTCD BAP sequence \\(387 bp) is first amplified by PCR from pXa-1 plasmid \\(Promega, Madison, WI) as follows: for each reaction, add 5 \u00b5l  Pfu buffer \\(10x buffer). Set the conditions for the PCR as follows: an initial denaturation step at 95 <sup>o</sup>C for 3 min followed by 35 cycles of 30 sec 95 <sup>o</sup>C denaturation, 30 sec 48 <sup>o</sup>C annealing, 1 min 72 <sup>o</sup>C extension and a final extension step of 10 min"},
+            {"role": "assistant", "content": "(PSTCD BAP sequence | amplified by | PCR), (pXa-1 plasmid | used for | PCR amplification), (Pfu buffer | added in | PCR reaction), (PCR reaction | set with conditions of | initial denaturation step), (PCR reaction | set with conditions of | 35 cycles of denaturation, annealing, and extension), (PCR reaction | set with conditions of | final extension step)"},
             {"role": "user", "content": "Reagents List: " + ", ".join(reagents)},
             {"role": "user", "content": "Equipments List: " + ", ".join(equipments)},
-            {"role": "user", "content": "Procedure: " + "Polymerase chain reaction. The PSTCD BAP sequence \\(387 bp) is first amplified by PCR from pXa-1 plasmid \\(Promega, Madison, WI) as follows: for each reaction, add 5 \u00b5l  Pfu buffer \\(10x buffer), 2.5 \u00b5l of each upstream and downstream primer \\(10 \u03bcM), 1.5 \u00b5l dNTPs mix \\(5 mM), 1 \u00b5l  pXa-1 \\(20 mg/L), 5 \u00b5l DMSO \\(Critical, PCR of BAP is not successful without it), 1 \u00b5l Pfu DNA polymerase \\(2.5 units/\u00b5l) and increase the volume to 50 \u00b5l with autoclaved ddH<sub>2</sub>O. Set the conditions for the PCR as follows: an initial denaturation step at 95 <sup>o</sup>C for 3 min followed by 35 cycles of 30 sec 95 <sup>o</sup>C denaturation, 30 sec 48 <sup>o</sup>C annealing, 1 min 72 <sup>o</sup>C extension and a final extension step of 10 min"},
+            {"role": "user", "content": "Procedure: " + procedure}
         ],
-        max_tokens=3000,
+        max_tokens=2500,
         temperature=0,
         top_p=1,
         frequency_penalty=0.0,
         presence_penalty=0.0,
         stop=["END"]
     )
-    print(response['choices'][0]['message']['content'])
+    return parse_relation_response(response['choices'][0]['message']['content'][1: -2])
+
+
+def is_meaningful(str):
+    print(str)
+    if len(str) < 10:
+        return False
+    else:
+        return True
 
 
 if __name__ == "__main__":
@@ -76,5 +94,11 @@ if __name__ == "__main__":
         procedure_text = data["content"][3]["content"]
         reagents = get_reagents(reagents_text)
         equipments = get_equipments(equipments_text)
-        relations = get_relations(reagents, equipments, procedure_text)
-
+        procedure_divided = procedure_text.split('\n')
+        relations = []
+        for procedure in procedure_divided:
+            if is_meaningful(procedure):
+                relations_divided = get_relations(reagents, equipments, procedure)
+                for relation_divided in relations_divided:
+                    relations.append(relation_divided)
+        print(relations)
